a3902eaf3f49158f08ae65f517ead8e0c3572963
Tom Mornini <tmornini@me.com>
2026-04-17 11:04:26 -0700

    debug: global __fit

--- web-app/app/flow-layout.ts
+++ web-app/app/flow-layout.ts
@@ -751,10 +751,9 @@ function fitToCanvas(
     const natH = maxY - minY;
     const posCx = (minX + maxX) / 2;
     const posCy = (minY + maxY) / 2;
-    // eslint-disable-next-line no-console
-    console.log('[FIT]', {
-        canvasW, canvasH, natW, natH,
-    });
+    (window as unknown as {
+        __fit?: object;
+    }).__fit = { canvasW, canvasH, natW, natH };
 
     const graphLandscape =
         (natW + NODE_WIDTH) >= (natH + NODE_HEIGHT);
